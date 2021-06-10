76e6d56a38a54d86eb336e8cd1894c6ad6b50343
Vishwas R <vishwas.r.jnv@gmail.com>
2021-06-10 18:10:01 +0530

    Added Scripts

---
+++ script.js
@@ -0,0 +1,10 @@
+/*
+  "name": "YouTube AdSkipper",
+  "short_name": "YouTube AdSkipper",
+  "description": "Skip YouTube ads automatically as soon as YouTube provides a skip button.",
+  "version": "1.0",
+  "author": "Vishwas R",
+*/
+(function(){function e(a){if(a.fireEvent)a.fireEvent("onclick");else{var b=document.createEvent("Events");b.initEvent("click",!0,!1);a.dispatchEvent(b)}}function g(){clearTimeout(f);m()||(f=setTimeout(function(){h();g()},2E3))}function n(a){return a.map(function(b){return Array.from(document.getElementsByClassName(b))||[]}).reduce(function(b,c){return b.concat(c)},[])}function p(a){var b=null,c,k=function(){for(var d=a;null!==d;){if("none"===d.style.display)return d;d=d.parentElement}return null}();
+k&&a!==b&&(c&&b&&(c.disconnect(),e(b)),c||(c=new MutationdomTreeObserver(function(){null!==b.offsetParent&&(e(b),b=void 0,c.disconnect())})),b=a,c.observe(k,{attributes:!0}))}function h(){n(classList).forEach(function(a){null===a.offsetParent?p(a):e(a)})}function m(){if(!("MutationdomTreeObserver"in window))return!1;var a=function(b){return b&&b[0]}(document.getElementsByTagName("ytd-player"));if(!a)return!1;(new MutationdomTreeObserver(function(){h()})).observe(a,{childList:!0,subtree:!0});clearTimeout(f);
+return!0}var f;try{var l=window.self!==window.top}catch(a){l=!0}l||g()})();
---
+++ script.src.js
@@ -0,0 +1,124 @@
+(function () {
+  var timerId;
+  var isInIframe = (function() {
+    try {
+      return (window.self !== window.top);
+    } catch (e) {
+      return true;
+    }
+  })();
+  
+  if (!isInIframe)
+    initTimeout();
+  
+  function simulateClick(el) {
+    var eventType = 'click';
+	
+	if (el.fireEvent) {
+		el.fireEvent('on' + eventType);
+	} else {
+		var evObj = document.createEvent('Events');
+		evObj.initEvent(eventType, true, false);
+		el.dispatchEvent(evObj);
+	}
+  }
+
+  function initTimeout() {
+    clearTimeout(timerId);
+
+    if (initDomTreeObserver()) {
+      return;
+    }
+    timerId = setTimeout(function() {
+      checkAndClickButtons();
+
+      initTimeout();
+    }, 2000);
+  }
+
+  function existingButtons(classNames) {
+    return classNames.map(name => {
+        return Array.from(document.getElementsByClassName(name)) || [];
+      }).reduce(function(acc, elems) {
+        return acc.concat(elems);
+      }, []);
+  }
+
+  function isElementVisible(ele) {
+    return ele.offsetParent === null ? false : true;
+  }
+
+  function simulateClickOnVisible(button) {
+	var skipButton = null, skipButtonDomTreeObserver;
+    var hiddenElement = (function() {
+      var currentElement = button;
+      while (currentElement !== null) {
+        if (currentElement.style.display === 'none')
+          return currentElement;
+
+        currentElement = currentElement.parentElement;
+      }
+
+      return null;
+    })();
+
+    if (!hiddenElement || button === skipButton)
+      return;
+	
+    if (skipButtonDomTreeObserver && skipButton) {
+      skipButtonDomTreeObserver.disconnect();
+      simulateClick(skipButton);
+    }
+
+    if (!skipButtonDomTreeObserver) {
+      skipButtonDomTreeObserver = new MutationdomTreeObserver(function() {
+        if (!isElementVisible(skipButton)) {
+          return;
+        }
+
+        simulateClick(skipButton);
+        skipButton = undefined;
+        skipButtonDomTreeObserver.disconnect();
+      });
+    }
+
+    skipButton = button;
+    skipButtonDomTreeObserver.observe(hiddenElement, { attributes: true });
+  }
+
+  function checkAndClickButtons() {
+    existingButtons(classList).forEach(button => {
+      if (!isElementVisible(button)) {
+        simulateClickOnVisible(button);        
+        return;
+      } 
+
+      simulateClick(button);
+    })
+  }
+  
+  function initDomTreeObserver() {
+    if (!('MutationdomTreeObserver' in window)) {
+      return false;
+    }
+
+    var ytPlayer = (function(nodeList) {
+      return nodeList && nodeList[0];
+    })(document.getElementsByTagName('ytd-player'));
+
+    if (!ytPlayer) {
+      return false;
+    }
+
+    var domTreeObserver = new MutationdomTreeObserver(function() {
+      checkAndClickButtons();
+    });
+
+    domTreeObserver.observe(ytPlayer, { childList: true, subtree: true });
+
+    clearTimeout(timerId);
+
+    return true;
+  }
+
+})();
